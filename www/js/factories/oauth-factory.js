6e61522581aa1b298e398479830567cf3b80786a
Daniel <daniel.s.roden@gmail.com>
2015-11-12 00:33:40 +0100

    linkedin factory

--- www/js/factories/oauth-factory.js
+++ www/js/factories/oauth-factory.js
@@ -5,8 +5,7 @@
   .factory('Oauth', ['$http', '$window', '$interval', '$cookies', 'nodeOrigin', '$timeout', function($http, $window, $interval, $cookies, nodeOrigin, $timeout){
 
      function authentication(target, cb){
-      var access_token,
-          account_email,
+      var accountEmail,
           popup = $window.open(nodeOrigin + '/auth/linkedin', target, 'top=' + screen.width/2 - 200 + ',left=' + screen.height/2 - 250 + ',width='+ screen.width + ',height=' + screen.height);
       popup.addEventListener('loadstop', function(location){
       var url = document.createElement('a');
@@ -19,9 +18,9 @@
               'code': "localStorage.getItem('linkedin')"
             }, function(values){
                 console.log('values', values);
-                account_email = values[0];
+                accountEmail = values[0];
                 popup.close();
-                cb(account_email);
+                cb(accountEmail);
             });
           }, 200);
         }
